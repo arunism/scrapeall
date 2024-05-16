f889790c9e77bae20812a0e419cd0d7ba3b366c4
arunism <thearunism@gmail.com>
2024-05-16 13:36:56 +0545

    ADD: Functionality to get_page content and extract all urls from given page

--- parse.py
+++ parse.py
@@ -1,5 +1,8 @@
-from pyppeteer import launch
+import asyncio
+from itertools import chain
 from omegaconf import OmegaConf
+from pyppeteer import launch
+from bs4 import BeautifulSoup
 
 
 config = OmegaConf.load("config.yaml")
@@ -18,3 +21,26 @@ class HTMLParser:
     async def close_browser(self) -> None:
         if self.browser:
             await self.browser.close()
+
+    async def get_page(self, url: str):
+        page = await self.browser.newPage()
+        await page.goto(url)
+        content = await page.content()
+        self.page = BeautifulSoup(content, "html.parser")
+
+    async def get_all_urls(self, url: str = None):
+        elements = list()
+        url = url or str(self.config["base"]["url"])
+        await self.get_page(url)
+        elements.extend(
+            chain.from_iterable(
+                self.page.select(path) for path in self.config["base"]["path"]
+            )
+        )
+        for element in elements:
+            text = element.text.strip()
+            link = element.get("href")
+            if text and link:
+                if "https" not in link:
+                    link = "https://www.masta-travel-health.com/" + link
+                self.urls[text] = link
