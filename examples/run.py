ca09905b2b9d12c5cf239a763a983d4994b36c88
arunism <thearunism@gmail.com>
2024-08-27 15:20:41 +0545

    ADD: Examples

---
+++ examples/run.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from tqdm import tqdm
+
+from scrapeall.utils import save_data
+from scrapeall.parse import HTMLParser
+
+
+async def main(vendor: str, config_path: str, output_file: str = ""):
+    data = dict()
+
+    parser = HTMLParser(vendor, config_path)
+    await parser.initialize_browser()
+    await parser.get_all_urls()
+    for key, url in tqdm(parser.urls.items()):
+        await parser.get_content(url)
+        data[key] = parser.data
+
+    await parser.close_browser()
+    if output_file:
+        await save_data(filename=output_file, data=data)
+    return data
+
+
+if __name__ == "__main__":
+    VENDOR = "quotestoscrape"
+    CONFIG = "examples/config.yaml"
+    OUTPUT_FILE = "output.json"
+    data = asyncio.run(main(VENDOR, CONFIG, OUTPUT_FILE))
