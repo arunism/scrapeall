73697b16bfb3c48195f81cab01ec2a3baecf921d
SaimonDahal-02 <saimondahal66@gmail.com>
2025-06-16 15:58:08 +0545

    FEAT: Add argparse CLI support with mode option and refactor main entrypoint

--- examples/run.py
+++ examples/run.py
@@ -1,15 +1,18 @@
+import argparse
 import asyncio
 
 from tqdm import tqdm
 
-from scrapeall.utils import save_data
 from scrapeall.parse import HTMLParser
+from scrapeall.utils import ProcessMode, save_data
 
 
-async def main(vendor: str, config_path: str, output_file: str = ""):
+async def main(
+    vendor: str, config_path: str, MODE: ProcessMode, output_file: str = ""
+) -> dict:
     data = dict()
 
-    parser = HTMLParser(vendor, config_path)
+    parser = HTMLParser(vendor, config_path, MODE)
     await parser.initialize_browser()
     await parser.get_all_urls()
     for key, url in tqdm(parser.urls.items()):
@@ -23,7 +26,27 @@ async def main(vendor: str, config_path: str, output_file: str = ""):
 
 
 if __name__ == "__main__":
-    VENDOR = "quotestoscrape"
-    CONFIG = "examples/config.yaml"
-    OUTPUT_FILE = "output.json"
-    data = asyncio.run(main(VENDOR, CONFIG, OUTPUT_FILE))
+    parser = argparse.ArgumentParser(
+        description="Run the processing with vendor, config, and optional mode/output."
+    )
+    parser.add_argument("--vendor", required=True, help="Your vendor name (required)")
+    parser.add_argument(
+        "--config",
+        required=True,
+        help="Path to your YAML configuration file (required)",
+    )
+    parser.add_argument(
+        "--mode",
+        choices=[m.value for m in ProcessMode],
+        default="both",
+        help="Processing mode (default: both)",
+    )
+    parser.add_argument(
+        "--output", default="output.json", help="Output filename (default: output.json)"
+    )
+
+    args = parser.parse_args()
+
+    mode = ProcessMode(args.mode)
+
+    data = asyncio.run(main(args.vendor, args.config, mode, args.output))
